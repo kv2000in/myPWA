73febf6cf733677913e76c8795aada171280eecc
kv2000in <kv2000inn@gmail.com>
2024-07-13 18:39:31 -0400

    service worker test

--- sw.js
+++ sw.js
@@ -13,6 +13,49 @@ const APP_STATIC_RESOURCES = [
  
 ];
 
+
+
+const putInCache = async (request, response) => {
+  const cache = await caches.open("v1");
+  await cache.put(request, response);
+};
+
+const cacheFirst = async ({ request, fallbackUrl }) => {
+  // First try to get the resource from the cache.
+  const responseFromCache = await caches.match(request);
+  if (responseFromCache) {
+    return responseFromCache;
+  }
+
+  // If the response was not found in the cache,
+  // try to get the resource from the network.
+  try {
+    const responseFromNetwork = await fetch(request);
+    // If the network request succeeded, clone the response:
+    // - put one copy in the cache, for the next time
+    // - return the original to the app
+    // Cloning is needed because a response can only be consumed once.
+    putInCache(request, responseFromNetwork.clone());
+    return responseFromNetwork;
+  } catch (error) {
+    // If the network request failed,
+    // get the fallback response from the cache.
+    const fallbackResponse = await caches.match(fallbackUrl);
+    if (fallbackResponse) {
+      return fallbackResponse;
+    }
+    // When even the fallback response is not available,
+    // there is nothing we can do, but we must always
+    // return a Response object.
+    return new Response("Network error happened", {
+      status: 408,
+      headers: { "Content-Type": "text/plain" },
+    });
+  }
+};
+
+
+
 // On install, cache the static resources
 self.addEventListener("install", (event) => {
   event.waitUntil(
@@ -42,7 +85,7 @@ self.addEventListener("activate", (event) => {
 
 // On fetch, intercept server requests
 // and respond with cached responses instead of going to network
-self.addEventListener("fetch", (event) => {
+/*self.addEventListener("fetch", (event) => {
   // As a single page app, direct app to always go to cached home page.
   if (event.request.mode === "navigate") {
     event.respondWith(caches.match("/"));
@@ -62,5 +105,15 @@ self.addEventListener("fetch", (event) => {
       return new Response(null, { status: 404 });
     })(),
   );
-});
+});*/
 
+
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    cacheFirst({
+      request: event.request,
+      fallbackUrl: "/pwa/icon-512.png",
+    }),
+  );
+});
